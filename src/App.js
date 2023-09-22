7aaabae48476cb8b4e0165b7b9debbc64e57027f
Zachary-Craig <zcraig100@gmail.com>
2023-09-21 21:24:11 -0400

    Working code for React-Boilerplate

--- src/App.js
+++ src/App.js
@@ -3,22 +3,42 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div class="card-container">
+      <div class="wrapper">
+        <div class="card">
+          <img class="image"></img>
+            src="https://logos-world.net/wp-content/uploads/2020/05/Pittsburgh-Penguins-symbol.jpg"
+            alt="Penguins Logo" width="400" height="225"
+            <div class="header">
+              <h3>Pittsburgh Penguins National Hockey Team</h3>
+            </div>
+            <h4>Description</h4>
+            <div class="description">
+              <h5>The Pittsburgh Penguins are an NHL hockey team from Pittsburgh, known for their championships and star players like Sidney Crosby and Mario Lemieux.</h5>
+            </div>
+            <div class="btn-wrapper">
+              <button onclick="hideDescription()">Details</button>
+            </div>
+          </div>
+      </div>
     </div>
+    <div class="button-box">
+        <div class="dup-button">
+          <button>Duplicate</button>
+        </div>
+
+        <div class="backgroundcolorButton">
+          <button onclick="changeBackgroundColor()"> Change Background Color</button>
+        </div>
+
+        <div class="headingButton">
+          <button onclick="changeHeading()"> Change Heading</button>
+        </div>
+
+        <div class="deleteCard">
+          <button onclick="deleteDupCard()">Delete</button>
+        </div>
+      </div></>
   );
 }
 
